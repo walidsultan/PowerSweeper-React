b939ef19cc2d44390e84b67f24bc6a5a00422973
Walid Sultan <walidalysultan@gmail.com>
2018-10-04 01:05:22 -0700

    Moved block value state to the board

--- src/scripts/components/board.tsx
+++ src/scripts/components/board.tsx
@@ -1,28 +1,49 @@
 import * as React from "react";
 import Block from './block';
 import BoardInterface from '../interfaces/BoardInterface';
-import BoardSettings from '../dto/boardSettings';
+import BoardSettings from '../states/BoardState';
 import { MineType } from "../enums/mineType";
+import BoardState from "../states/BoardState";
 
-export default class Board extends React.Component<BoardInterface, {}> {
+export default class Board extends React.Component<BoardInterface, BoardState > {
         private mines: number[][];
 
         constructor(props) {
                 super(props)
                 this.initializeMines();
-                // this.state = new BoardSettings();
+                this.initializeValues();
+
+                  //Mark mines
+                  this.AddMines(this.props.smallMinesCount, MineType.Small);
+                  this.AddMines(this.props.mediumMinesCount, MineType.Medium);
+                  this.AddMines(this.props.bigMinesCount, MineType.Large);
+                  console.log(this.mines);
+        }
+
+        initializeValues(){
+                let values = this.getDoubleArray(this.props.levelWidth,this.props.levelHeight);
+                this.state={Values:values};
         }
 
         initializeMines() {
-                this.mines = Array.apply(null, Array(this.props.levelWidth)).map(Number.prototype.valueOf, 0);
-                for (let i in this.mines) {
-                        this.mines[i] = Array.apply(null, Array(this.props.levelHeight)).map(Number.prototype.valueOf, 0);
-                }
-                console.log(this.mines);
+                this.mines = this.getDoubleArray(this.props.levelWidth,this.props.levelHeight);
+              
         }
 
-        handleClick(left, top, value) {
-                alert(left + " : " + top + " : " + value);
+        getDoubleArray(width:number,height:number){
+                let newArray= Array.apply(null, Array(width)).map(Number.prototype.valueOf, 0);
+                for (let i in newArray) {
+                        newArray[i] = Array.apply(null, Array(height)).map(Number.prototype.valueOf, 0);
+                }
+                return newArray;
+        }
+
+        handleBlockClick(left, top) {
+              let value = this.mines[left-1][top]+this.mines[left+1][top] + this.mines[left][top-1]+this.mines[left][top+1] +
+                                this.mines[left-1][top-1]+this.mines[left+1][top+1] + this.mines[left-1][top+1]+this.mines[left+1][top-1]      ;
+              let values =this.state.Values;
+              values[left][top]=value;
+              this.setState({Values:values});
         }
 
         handleRightClick(left, top) {
@@ -45,22 +66,16 @@ export default class Board extends React.Component<BoardInterface, {}> {
         generatePuzzle(levelWidth: number, levelHeight: number): any[] {
                 let puzzle = [];
 
-                //Mark mines
-                this.AddMines(this.props.smallMinesCount, MineType.Small);
-                this.AddMines(this.props.mediumMinesCount, MineType.Medium);
-                this.AddMines(this.props.bigMinesCount, MineType.Large);
-
                 //Add blocks
                 for (let i = 0; i < levelWidth; i++) {
                         for (let j = 0; j < levelHeight; j++) {
-                                let value: number = Math.floor(Math.random() * 3);
-                                puzzle.push(<Block
+                                puzzle.push(<Block 
                                         Left={i}
                                         Top={j}
-                                        Value={value}
                                         BlockSize={80}
-                                        onClick={() => this.handleClick(i, j, value)}
+                                        onClick={() => this.handleBlockClick(i, j)}
                                         onContextMenu={() => this.handleRightClick(i, j)}
+                                        Value={this.state.Values[i][j]}
                                         HasMine={this.mines[i][j] > 0 ? true : false}
                                         Mine={this.mines[i][j] > 0 ? this.mines[i][j] : null} />);
                         }
